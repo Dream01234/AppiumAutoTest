b6e770c549907771322ea561e555f40d990ce640
Dream01234 <417081473@qq.com>
2020-12-22 16:31:42 +0800

    优化基础函数Enlement

--- Enlement.py
+++ Enlement.py
@@ -185,35 +185,6 @@ class operation:
                 boss = boss + 1
                 print("------------找不到目标:"+tager+",滚吧!+"+str(boss))
 
-    def find_uiautomator_tager_click2(driver, tager, wait):
-        try:
-            operation.element_exit_is(driver, tager, wait)
-
-            if driver.find_element_by_android_uiautomator(tager):
-                driver.find_element_by_android_uiautomator(tager).click()
-                print("找到目标:" + tager)
-        except Exception as e:
-            print("找不到该元素")
-
-
-    def find_uiautomator_tager_send_keys2(driver, tager, something, wait):
-        """使用uiautomator根据tager输入文本
-
-               driver:设备
-               tager:目标元素
-               time:滑动次数
-               something: 输入文本内容
-               wait:等待时间(秒)
-            """
-        try:
-                operation.element_exit_is(driver, tager, wait)
-                if driver.find_element_by_android_uiautomator(tager):
-                    driver.find_element_by_android_uiautomator(tager).send_keys(something)
-                    print("找到目标:"+tager)
-        except Exception as e:
-                print("------------找不到目标:"+tager)
-
-
     def access_visit_permissions(driver):
         """登录时获取访问权限(真机调试用)"""
         i = 0
