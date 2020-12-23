0cf17b07bd7949f2bb781b163d94fc9fd85b6f7a
Dream01234 <417081473@qq.com>
2020-12-23 18:39:56 +0800

    读取excel文件里面的内容，进行模板化（test_demo3）

--- test_demo3.py
+++ test_demo3.py
@@ -8,21 +8,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
-caps = {}
-caps["platformName"] = "Android"
-caps["platformVersion"] = "7.1.2"
-caps["deviceName"] = "MI_9"
-caps["appPackage"] = "cn.smartinspection.combine"
-caps["appActivity"] = "cn.smartinspection.login.ui.activity.WelcomeGuideActivity"
-caps["ensureWebviewsHavePages"] = True
-caps["automationName"] = "Uiautomator2"
-driver = webdriver.Remote("http://localhost:4723/wd/hub",caps)
+# caps = {}
+# caps["platformName"] = "Android"
+# caps["platformVersion"] = "7.1.2"
+# caps["deviceName"] = "MI_9"
+# caps["appPackage"] = "cn.smartinspection.combine"
+# caps["appActivity"] = "cn.smartinspection.login.ui.activity.WelcomeGuideActivity"
+# caps["ensureWebviewsHavePages"] = True
+# caps["automationName"] = "Uiautomator2"
+# driver = webdriver.Remote("http://localhost:4723/wd/hub",caps)
 
 
 wb = load_workbook('D:/自动化模板.xlsx')
 print(wb.sheetnames)
 ws = wb['实测实量']
 one_cel = ws.cell(row=1, column=2).value
-print(one_cel)
+two_cel = ws['A1'].value
+three_cel = ws['B1'].value
+four_cel = two_cel + three_cel
 
-Enlement.operation.find_uiautomator_tager_click(driver, one_cel, 3, 3)
+print(one_cel)
+print(two_cel)
+print(four_cel)
+# Enlement.operation.find_uiautomator_tager_click(driver, one_cel, 3, 3)
