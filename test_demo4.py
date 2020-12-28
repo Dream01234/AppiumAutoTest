6281efbe631e69a739613003da88ab2478c4fbfc
Dream01234 <417081473@qq.com>
2020-12-28 18:52:19 +0800

    完善用例（销项）

--- test_demo4.py
+++ test_demo4.py
@@ -17,8 +17,8 @@ caps["ensureWebviewsHavePages"] = True
 caps["automationName"] = "Uiautomator2"
 driver = webdriver.Remote("http://localhost:4723/wd/hub",caps)
 
+# 实测设置爆点
 # Enlement.operation.access_visit_permissions(driver)
-
 # Enlement.operation.find_uiautomator_tager_click(driver, 'text("马上登录")', 3, 3)
 # Enlement.operation.find_uiautomator_tager_send_keys(driver, 'text("请输入用户名")', 3, 'kentestgrp10', 3)
 # Enlement.operation.find_uiautomator_tager_send_keys(driver ,'text("请输入密码")', 3, '12345678',3)
@@ -60,7 +60,7 @@ driver = webdriver.Remote("http://localhost:4723/wd/hub",caps)
 # Enlement.operation.find_uiautomator_tager_click(driver, 'text("我")', 5, 3)
 # Enlement.operation.find_uiautomator_tager_click(driver, 'text("切换帐号")', 5, 3)
 
-
+# 整改修复
 # Enlement.operation.find_uiautomator_tager_click(driver, 'text("马上登录")', 3, 3)
 # Enlement.operation.find_uiautomator_tager_send_keys(driver, 'text("请输入用户名")', 3, 'kentestgrp10', 3)
 # Enlement.operation.find_uiautomator_tager_send_keys(driver ,'text("请输入密码")', 3, '12345678',3)
@@ -78,10 +78,34 @@ driver = webdriver.Remote("http://localhost:4723/wd/hub",caps)
 # Enlement.operation.find_uiautomator_tager_click(driver, 'text("19#测试1")', 5, 3)
 # Enlement.operation.find_uiautomator_tager_click(driver, 'text("爆点清单")', 5, 3)
 # Enlement.operation.find_uiautomator_tager_click(driver, 'text("问题清单")', 5, 3)
-# Enlement.operation.find_uiautomator_tager_click(driver, 'textContains("19# 71")', 5, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver, 'textContains("检查组")', 5, 3)
 # Enlement.operation.find_uiautomator_tager_click(driver, 'text("完成整改")', 5, 3)
 # Enlement.operation.find_uiautomator_tager_click(driver, 'text("数值1")', 5, 3)
 # Enlement.operation.find_uiautomator_tager_send_keys(driver, 'text("数值1")', 5, '1', 3)
 # Enlement.operation.find_uiautomator_tager_click(driver, 'text("数值2")', 5, 3)
 # Enlement.operation.find_uiautomator_tager_send_keys(driver, 'text("数值2")', 5, '2', 3)
 # Enlement.operation.find_uiautomator_tager_click(driver, 'text("确定")', 5, 3)
+
+# 销项通过
+# Enlement.operation.find_uiautomator_tager_click(driver, 'text("马上登录")', 3, 3)
+# Enlement.operation.find_uiautomator_tager_send_keys(driver, 'text("请输入用户名")', 3, 'kentestgrp10', 3)
+# Enlement.operation.find_uiautomator_tager_send_keys(driver ,'text("请输入密码")', 3, '12345678',3)
+# Enlement.operation.find_uiautomator_tager_send_keys(driver ,'text("企业编码(非必填)")', 3, 't8',3)
+# Enlement.operation.find_uiautomator_tager_click(driver,'text("登录")', 3, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver,'text("ken集团")', 3, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver,'text("1kentest公司3").fromParent(text("向下展开"))', 5, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver,'text("公司3项目1")', 5, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver,'text("更多")', 5, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver,'text("实测实量")', 5, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver,'text("全部同步")', 5, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver, 'resourceId("android:id/text1").text("19#测试1")', 5, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver, 'resourceId("android:id/button1").text("确定")', 5, 3)
+# Enlement.operation.wait_loading(driver, 'text("停止同步")', 'text("全部同步")')
+# Enlement.operation.find_uiautomator_tager_click(driver, 'text("19#测试1")', 5, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver, 'text("爆点清单")', 5, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver, 'textContains("待销项")', 3, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver, 'text("销项")', 3, 3)
+# Enlement.operation.find_uiautomator_tager_click(driver, 'text("确定")', 3, 3)
+# Enlement.operation.is_toast_exist(driver, '操作成功')
+
+
