55c2136613bccbed8c36f51d7db68ad4b6bb45e4
Dream01234 <417081473@qq.com>
2021-10-21 11:33:55 +0800

    page object模式 1.新增loginel类

---
+++ login.py
@@ -0,0 +1,27 @@
+class Loginel:
+    "马上登录"
+    loginNow = 'cn.smartinspection.combine:id/btn_go_to_login'
+
+    "用户名"
+    user_name = 'cn.smartinspection.combine:id/et_login_username'
+
+    "密码"
+    pwd = 'cn.smartinspection.combine:id/et_login_pwd'
+
+    "企业编码"
+    business_code = 'cn.smartinspection.combine:id/et_enterprise_id'
+
+    "隐私协议"
+    agree = 'cn.smartinspection.combine:id/cb_select'
+
+    "登录"
+    loginButoon = 'cn.smartinspection.combine:id/btn_login'
+
+    "注册"
+    register = 'cn.smartinspection.combine:id/tv_register'
+
+    "重置密码"
+    reset_pwd = 'cn.smartinspection.combine:id/tv_reset_password'
+
+    "手机验证码登录"
+    code = 'cn.smartinspection.combine:id/tv_reset_password'
