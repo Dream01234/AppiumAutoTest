17388438eb3d6d42047118e86a53148321890613
Dream01234 <417081473@qq.com>
2021-10-29 12:15:02 +0800

    page object模式 登录类集成了授权操作

--- login_operation.py
+++ login_operation.py
@@ -1,9 +1,12 @@
 import login
+import index_operation
 class loginpage_control:
 
     "登录智建云(不带企业编码)"
     def logincloud1(self,username,pwd):
+        a = index_operation.index_control()
         t = login.Loginel()
+        a.click_loginNow()
         t.sendUsename(username)
         t.sendPwd(pwd)
         t.checkAgree()
@@ -11,7 +14,9 @@ class loginpage_control:
 
     "登录智建云（带企业编码）"
     def logincloud2(self,username,pwd,code):
+        a = index_operation.index_control()
         t = login.Loginel()
+        a.click_loginNow()
         t.sendUsename(username)
         t.sendPwd(pwd)
         t.sendBusinesscode(code)
