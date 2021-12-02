2b9114c7ee4dc332847617e8b07de91ee8b9a62a
Dream01234 <417081473@qq.com>
2021-12-02 17:54:21 +0800

    page object模式 新增图纸类和任务操作方法封装

--- task_operation.py
+++ task_operation.py
@@ -6,4 +6,17 @@ class picture_control:
         t = task.picture
         t.clickarea(area)
 
-    "切换"
+    "切换测量"
+    def selectmeasure(self,measure):
+        t = task.picture
+        t.clickmeasure(measure)
+
+    "切换爆点"
+    def selectboom(self,boom):
+        t = task.picture
+        t.clickmeasure(boom)
+
+    "选择指标"
+    def selecttarget(self,indicators):
+        t = task.picture
+        t.clickindicators(indicators)
